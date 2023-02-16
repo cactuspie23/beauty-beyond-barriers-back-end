c4b0038cba91818e1496e423cc7eb61f0a84ab33
AlyssaD <pink_lime@live.com>
2023-02-16 10:29:04 -0500

    set up models

---
+++ models/cart.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const cartSchema = new Schema({
+  products: [{
+    productId: { type: Schema.Types.ObjectId, ref: 'Product' },
+    quantity: { type: Number, required: true, min: 1, default: 1 },
+  }],
+},{
+  timestamps: true,
+})
+
+const Cart = mongoose.model('Cart', cartSchema)
+
+export { Cart }
---
+++ models/product.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+  name: String,
+  category: String,
+  description: String,
+  url: String,
+  imgUrl: String,
+  price: Number,
+  stock: { type: Number, required: true, min: 1, default: 1 },
+},{
+  timestamps: true,
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+export { Product }

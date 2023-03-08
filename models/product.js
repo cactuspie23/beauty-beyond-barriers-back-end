777e66f3f21c8d4190cca226cfa5b795805a55d9
AlyssaD <pink_lime@live.com>
2023-03-08 12:30:30 -0500

    update product model

--- models/product.js
+++ models/product.js
@@ -5,8 +5,10 @@ const Schema = mongoose.Schema
 const productSchema = new Schema({
   name: String,
   category: String,
+  shortDescription: String,
   description: String,
   imgUrl: String,
+  size: String,
   price: Number,
   stock: { type: Number, required: true, min: 1, default: 1 },
 },{
